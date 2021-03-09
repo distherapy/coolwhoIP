ed55f85d47be438fc928e9fddc33ad5341f7d220
Brent Elisens <56242810+distherapy@users.noreply.github.com>
2021-03-09 18:37:30 -0500

    Update stu.py

--- stu.py
+++ stu.py
@@ -5,6 +5,7 @@ from urllib.request import urlopen
 
 ratchet = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ratchet.bind((socket.gethostname(), 80))
+#get_ip = request.environ['REMOTE_ADDR']  
 url = 'http://ipinfo.io/json'
 response = urlopen(url)
 data = json.load(response)
