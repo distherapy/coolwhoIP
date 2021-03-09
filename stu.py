8e3c3fe9e23a153e60762b1da2731f17c556c4fc
Brent Elisens <56242810+distherapy@users.noreply.github.com>
2021-03-09 10:45:19 -0500

    Update stu.py

--- stu.py
+++ stu.py
@@ -1,6 +1,19 @@
 import socket
+import whois
+import json
+from urllib.request import urlopen
+
 ratchet = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ratchet.bind((socket.gethostname(), 80))
+url = 'http://ipinfo.io/json'
+response = urlopen(url)
+data = json.load(response)
+ip=data['ip']
+w = whois.whois('ip')
+
 def whoip():
   pass
+
 whoip()
+print(ip, w)
+
